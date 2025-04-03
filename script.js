f6adc420cfd8530a2b54a989b7c97bd644cdbc6c
Numbers <magar.samartha2@gmail.com>
2025-04-04 00:52:10 +0545

    complete-ui

---
+++ script.js
@@ -0,0 +1,25 @@
+const toggleAll = document.getElementById('toggle-all');
+const toggleActive = document.getElementById('toggle-active');
+const toggleInactive = document.getElementById('toggle-inactive');
+
+function allToogle() {
+    toggleAll.classList.add('active');
+    toggleActive.classList.remove('active');
+    toggleInactive.classList.remove('inactive');
+}
+
+function activeToogle() {
+    toggleAll.classList.remove('active');
+    toggleActive.classList.add('active');
+    toggleInactive.classList.remove('active');
+}
+
+function inactiveToggle() {
+    toggleAll.classList.remove('active');
+    toggleActive.classList.remove('active');
+    toggleInactive.classList.add('active');
+}
+
+toggleAll.addEventListener("click", allToogle);
+toggleActive.addEventListener("click", activeToogle);
+toggleInactive.addEventListener("click", inactiveToggle);
